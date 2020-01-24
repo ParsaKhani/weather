476d6e4e172b128b91b14c819a9ca90c4609536f
Parsa_Khani <parsakhani4@yahoo.com>
2020-01-24 21:25:58 +0330

    Updates

---
+++ precache-manifest.efc8d4c75866b4be46d7b0024d71edf0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ecc47ad1d191f17087fa4fc3ef9940e",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "fac8d40c66401fb69215",
+    "url": "/weather/static/js/2.9e8913ff.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/weather/static/js/2.9e8913ff.chunk.js.LICENSE"
+  },
+  {
+    "revision": "779a5619ff759f35a78e",
+    "url": "/weather/static/js/main.2750a05d.chunk.js"
+  },
+  {
+    "revision": "6fd9ca1c40336747ceeb",
+    "url": "/weather/static/js/runtime-main.644e92fa.js"
+  }
+]);
